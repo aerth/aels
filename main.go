1dc29947cae76286807874d801d77c6fdef86f34
aerth <aerth@riseup.net>
2018-02-15 21:26:18 -0800

    move all to main

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"flag"
 	"log"
-
-	"github.com/aerth/aels/lib/aels"
+	//"github.com/aerth/aels/lib/aels"
 )
 
 func main() {
 	log.SetFlags(log.Lshortfile)
 	configpath := flag.String("conf", "", "path to toml config file. use $PORT and $SECRET environment to skip config.")
 	flag.Parse()
-	l, err := aels.New(*configpath)
+	l, err := New(*configpath)
 	if err != nil {
 		log.Fatal(err)
 	}
